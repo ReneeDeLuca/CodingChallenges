0514ce305f37967cd9151eab027d196e4a063f3d
Renee DeLuca <renee.deluca@outlook.com>
2022-03-06 15:04:40 -0800

    add solution for coding challenge for 2022-03-06

--- CC 2022-03-06/app.js
+++ CC 2022-03-06/app.js
@@ -1,3 +1,22 @@
 // Sentence Smash
 // Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
 
+//P: Empty array should return empty string, one element array should return string with no spaces.  
+//R: String with spaces in between elements
+//E: ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+//   [] => ''
+//   ['coffee'] => 'coffee'
+//P: creat a function that parses through the array and pulls out each element
+function makeSentence(arr){
+    if(arr.length === 0){
+        return ''
+    }else if(arr.length === 1){
+        return arr.toString('') 
+    }else{
+        return arr.join(' ')
+    }
+}
+//   add to a string with spaces in between each element
+//   but not before or after. 
+
+//got it, don't need .toString on longer array, only need to join and give parameter for removing the commas. 
