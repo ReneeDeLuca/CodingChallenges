961138114bac1218e3125a973d6aa415fafbf0bc
Renee DeLuca <rdeluca631@gmail.com>
2023-02-16 14:41:32 -0800

    add coding challenge for 2023-02-16

---
+++ CC 2023 Feb/CC 2023-02-16/app.js
@@ -0,0 +1,16 @@
+// kyu: 8
+//title: Convert boolean values to strings 'Yes' or 'No'.
+//description: Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+//P:
+//R:
+//E:
+//P:
+
+
+//notes:
+
+
+//notes:
+
+//test:
