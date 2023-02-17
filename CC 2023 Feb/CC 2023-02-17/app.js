db29690c09179d195fff4aa8d68053532b426293
Renee DeLuca <rdeluca631@gmail.com>
2023-02-17 14:40:01 -0800

    add coding challenge for 2023-02-17

---
+++ CC 2023 Feb/CC 2023-02-17/app.js
@@ -0,0 +1,17 @@
+// kyu: 8
+//title: Calculate average
+//description: Write a function which calculates the average of the numbers in a given list.
+//Note: Empty arrays should return 0.
+
+//P:
+//R:
+//E:
+//P:
+
+
+//notes:
+
+
+//notes:
+
+//test:
