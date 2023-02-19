5eeb822cfe58d2b38ec5afc8bed487de74ced10c
Renee DeLuca <rdeluca631@gmail.com>
2023-02-19 14:23:01 -0800

    add coding challenge for CC 2023-02-19

---
+++ CC 2023-02-19/app.js
@@ -0,0 +1,16 @@
+// kyu: 7
+//title: Sort Arrays
+//description: Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+//P:
+//R:
+//E:
+//P:
+
+
+//notes:
+
+
+//notes:
+
+//test:
