40accc1c7b67481132257e4d2c03741188344bab
Renee DeLuca <rdeluca631@gmail.com>
2023-02-22 16:15:21 -0800

    add solutions and notes for coding challenge CC 2023-02-22

--- CC 2023 Feb/CC 2023-02-22/app.js
+++ CC 2023 Feb/CC 2023-02-22/app.js
@@ -16,15 +16,57 @@
 // 0 <= x <= 4
 // 0 <= y <= 4
 
-//P:
-//R:
-//E:
-//P:
+//P: always 10 matches, x and y are always between 0 and 4 inclusive.   
+//R: return sum of points awarded   
+//E: ["1:0","2:0","3:0","4:0","2:1","3:1","4:1","3:2","4:2","4:3"]
+//   ["1:1","2:2","3:3","4:4","2:2","3:3","4:4","3:3","4:4","4:4"]
+//   ["0:1","0:2","0:3","0:4","1:2","1:3","1:4","2:3","2:4","3:4"]
+//P: need to get the second character of each string in each element and use a conditional statement against the fourth character to awared points. 
 
+const points = games => {
+    let total = 0
+    total = games.forEach((item) => {
+        if(Number(item[0])-Number(item[2]) > 0){
+            console.log('Win!')
+            total += 3
+        }else if(Number(item[0])-Number(item[2]) === 0){
+            console.log('Tie!')
+            total += 1
+        }else{
+            console.log('Loss!')
+        }
+    })
+    return total
+}
+//notes: Was having trouble seeing what was going on, added console.log to find out what it was doing. " " are not included in the indexing of the string for the array elements. Did not know that. Once I figured out that it was not working correctly at the if and else if statements, it was an easy fix for that part, but still not adding to the total, resulting in 'undefined'. 
 
-//notes:
+let points2 = function(games){
+    let total = []
+    let pushTotal = games.map((str) => {
+        if(Number(str[0])-Number(str[2]) > 0){
+            total.push(3)
+            console.log('Win!')    
+        }else if(Number(str[0])-Number(str[2]) === 0){
+            total.push(1)
+            console.log('Tie!')    
+        }else{
+            total.push(0)
+            console.log('Loss!')
+        }
+    })
+    console.log(total)
+    return total.reduce((current, next) => current + next, 0)
+}
 
+//notes: needed .map instead of .forEach, hard to know when to use each one, but seems like .map is the way to go when you need something returned. 
 
-//notes:
+//notes: checked for simpler examples. while the example I found is shorter, it is hard to read to me, needs better variables to clearly explain what is happening. 
+
+// const points = games => games
+//                           .map(x => x[0] > x[2] ? 3 : x[0] === x[2] ? 1 : 0)
+//                           .reduce((p, c) => p + c)
 
 //test:
+console.log(points2(["1:0","2:0","3:0","4:0","2:1","3:1","4:1","3:2","4:2","4:3"]) , 30)
+console.log(points2(["1:1","2:2","3:3","4:4","2:2","3:3","4:4","3:3","4:4","4:4"]) , 10)
+console.log(points2(["0:1","0:2","0:3","0:4","1:2","1:3","1:4","2:3","2:4","3:4"]) , 0)
