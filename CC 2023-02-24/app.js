47ec7cec61035ecfed7b86a339353cc6803a7441
Renee DeLuca <rdeluca631@gmail.com>
2023-02-24 18:27:44 -0800

    add coding challenge for 2023-02-24

---
+++ CC 2023-02-24/app.js
@@ -0,0 +1,16 @@
+// kyu: 8
+//title: Square(n)Sum
+//description: Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+//P:
+//R:
+//E: For example, for [1, 2, 2] it should return 9
+//P:
+
+
+//notes:
+
+
+//notes:
+
+//test:
