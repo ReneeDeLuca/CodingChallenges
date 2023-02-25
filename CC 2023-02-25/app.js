e044810f105cf1f4dfbf8dff4d7c9bf41430b2c1
Renee DeLuca <rdeluca631@gmail.com>
2023-02-25 11:58:07 -0800

    update solution for CC 2023-02-25

--- CC 2023-02-25/app.js
+++ CC 2023-02-25/app.js
@@ -6,15 +6,21 @@
 // Numbers may be positive or negative, integers or floats.
 // Floats with decimal part non equal to zero are considered UNeven for this kata.
 
-//P:
-//R:
-//E:
-//P:
+//P: need to consider pos and neg numbers, floats, and zero
+//R: return true if even, else false
+//E: 2 -> true
+//   -18 -> true
+//   0.234 -> false
+//   17 -> false
+//P: %2 === 0 return true
+
+const testEven = num => num % 2 === 0
 
 
-//notes:
-
-
-//notes:
+//notes: easy peasy
 
 //test:
+console.log(testEven(2),true)
+console.log(testEven(-18),true)
+console.log(testEven(0.234),false)
+console.log(testEven(17),false)
