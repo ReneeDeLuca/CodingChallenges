32d525fe9508a3d3ed4518ebc9bad3b90be3b637
Renee DeLuca <rdeluca631@gmail.com>
2023-02-26 18:18:39 -0800

    add coding challenge for 2023-02-26

---
+++ CC 2023-02-26/app.js
@@ -0,0 +1,16 @@
+// kyu: 7
+//title: Filter the number
+//description: Oh, no! The number has been mixed up with the text. Your goal is to retrieve the number from the text, can you return the number back to its original state?You will be given a string of numbers and letters mixed up, you have to return all the numbers in that string in the order they occur.
+
+//P:
+//R: return a number from a string.
+//E:
+//P:
+
+
+//notes:
+
+
+//notes:
+
+//test:
