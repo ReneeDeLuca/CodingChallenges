e8c899dcb10d294601f885c5bf706af11c874b7f
Renee DeLuca <rdeluca631@gmail.com>
2023-02-27 18:22:12 -0800

    add notes for PREP

--- CC 2023-02-27/app.js
+++ CC 2023-02-27/app.js
@@ -1,11 +1,11 @@
-// kyu:
-//title:
-//description:
+// kyu: 8
+//title: Find Multiples of a Number
+//description: In this simple exercise, you will build a program that takes a value, integer , and returns a list of its multiples up to another value, limit . If limit is a multiple of integer, it should be included as well. There will only ever be positive integers passed into the function, not consisting of 0. The limit will always be higher than the base.
 
-//P:
-//R:
-//E:
-//P:
+//P: positive integers only, never 0 or negative
+//R: array of only the required numbers
+//E: if the parameters passed are (2, 6), the function should return [2, 4, 6] as 2, 4, and 6 are the multiples of 2 up to 6.
+//P: loop through and see if modulus === 0, if so add to array, only up to second paramenter. 
 
 
 //notes:
