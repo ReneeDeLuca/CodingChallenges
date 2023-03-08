385c7b8bc5040f83f94f113ee337bb7db8ca4279
Renee DeLuca <renee.deluca@outlook.com>
2023-03-07 16:23:11 -0800

    coding challenge with solution for 2023-03-07

---
+++ CC 2023-03-07/app.js
@@ -0,0 +1,50 @@
+// 8kyu Greet Me
+//Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+
+//P: whether input is lower, upper, or a mix, should return as a proper noun
+//R: return string with greeting and name with !
+//E: "riley" --> "Hello Riley!"
+//   "JACK"  --> "Hello Jack!"
+//P: constructor? not sure how to make it but I think this is the answer. 
+
+// look up on MDN:
+// constructor example:
+// class Person {
+//     constructor(name) {
+//       this.name = name;
+//     }
+  
+//     introduce() {
+//       console.log(`Hello, my name is ${this.name}`);
+//     }
+//   }
+  
+//   const otto = new Person("Otto");
+  
+//   otto.introduce(); // Hello, my name is Otto
+
+// not familiar with the 'class' portion of the example. going to review Mosh's OOP vod to review his constructor syntax.
+
+function Greeting(name){
+    this.name = function capitalize(name) {
+        return name[0].toUpperCase() + name.slice(1).toLowerCase();
+      }
+    this.greet = function greet(){
+        console.log(`Hello, ${this.name}!`)
+    }
+}
+
+//ok got this far and I think I'm over complicating this. Constructor is not needed.
+
+// try again
+
+let greeting = function capitalize(name) {
+    return 'Hello '+name[0].toUpperCase() + name.slice(1).toLowerCase()+'!';
+    
+}
+
+// ok this works on the test 'riley' but it could be cleaner
+
+const greet = name => 'Hello '+name[0].toUpperCase() + name.slice(1).toLowerCase()+'!'
+
+//works! good job, me. 
