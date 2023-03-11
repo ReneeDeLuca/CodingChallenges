daaabd79d04a397e84b9c24c66627418d354acbb
Renee DeLuca <rdeluca631@gmail.com>
2023-03-11 14:25:38 -0800

    adding solution and notes for CC 2023-03-11

--- CC 2023-03-11/app.js
+++ CC 2023-03-11/app.js
@@ -11,15 +11,28 @@
 // I is misinterpreted as 1
 // The test cases contain numbers only by mistake.
 
-//P:
-//R:
-//E:
-//P:
+//P: should only change 5, 0, & 1, will not include numbers to be kept. 
+//R: return the corrected word
+//E: "L0ND0N", "DUBL1N", "51NGAP0RE", "BUDAPE5T", "PAR15"
+//P: look through the string and replace the 5s with Ss, the 0 with Os, and the 1s with Is.
+
+const correct = (str) => {
+    str = str.replaceAll("5", "S");
+    str = str.replaceAll("0", "O");
+    str = str.replaceAll("1", "I");
+    return str
+}
 
 
-//notes:
+//notes: condense? use regex and /g instead of replaceAll and restating str =
+
+//const correct = string => string.replace(/0/g, "O").replace(/1/g, "I").replace(/5/g, "S");
 
 
-//notes:
 
 //test:
+console.log(correct("L0ND0N"), "LONDON")
+console.log(correct("DUBL1N"), "DUBLIN")
+console.log(correct("51NGAP0RE"), "SINGAPORE")
+console.log(correct("BUDAPE5T"), "BUDAPEST")
+console.log(correct("PAR1S"), "PARIS")
