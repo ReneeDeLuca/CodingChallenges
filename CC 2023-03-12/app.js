3c289f5d5a1537f87272ef39b7114690d239cc59
Renee DeLuca <rdeluca631@gmail.com>
2023-03-12 16:21:03 -0700

    update with notes and solution for CC 2023-03-12

--- CC 2023-03-12/app.js
+++ CC 2023-03-12/app.js
@@ -4,15 +4,29 @@
 // Complete the method which accepts such an array, and returns that single different number.
 // The input array will always be valid! (odd-length >= 3)
 
-//P:
-//R:
-//E:
-//P:
+//P: array length always odd and always >= 3
+//R: return the different number
+//E: [1,1,2],[1,2,1],[2,1,1],[17, 17, 3, 17, 17, 17, 17]
+//P: iterate over the array and compare numbers. how to figure out which one is the different number? what if I map to two different arrays depending on the comparison (true/false), then return the value of array[0], only if the length is 1. 
 
+function stray(numbers) {
+    while(numbers.length > 2){
+        if(numbers[0] === numbers[1] && numbers[0] !== numbers[2]){
+            return numbers[2]
+        }else if(numbers[0] !== numbers[1] && numbers[0] === numbers[2]){
+            return numbers[1]
+        }else if (numbers[0] !== numbers[1] && numbers[0] !== numbers[2]){
+            return numbers[0]
+        }else{
+            numbers.shift()
+        }
+    }               
+}
 
-//notes:
-
-
-//notes:
+//notes: keep comparing the first three, then remove the first if they are all the same, otherwise return the one that is different. 
 
 //test:
+console.log(stray([1, 1, 2]), 2)
+console.log(stray([1, 2, 1]), 2)
+console.log(stray([2, 1, 1]), 2)
+console.log(stray([17, 17, 3, 17, 17, 17, 17]))
